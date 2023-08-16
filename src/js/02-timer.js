4901ed149b97111c45dc2db84ba3a1d55eb02195
Dmytro Prontenko <dptontenko@gmail.com>
2023-08-16 15:57:49 +0200

    #2 done

---
+++ src/js/02-timer.js
@@ -0,0 +1,77 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import language from 'flatpickr/dist/l10n/uk';
+import { convertMs } from './helpers';
+import Notiflix from 'notiflix';
+import { convertMs } from './helpers';
+
+const refs = {
+  datePicker: document.querySelector('#datetime-picker'),
+  startCountdown: document.querySelector('button'),
+  timer: document.querySelector('.timer'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+let isActive = false;
+let selectedDate = '';
+
+refs.startCountdown.addEventListener('click', onStartCountdownClick);
+
+flatpickr(refs.datePicker, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(), //встановлює поточну дату в інпут
+  minuteIncrement: 1,
+  locale: 'uk',
+
+  onClose(selectedDates) {
+    const currentDate = new Date().setSeconds(0, 0);
+    selectedDate = selectedDates[0];
+    console.log(selectedDates[0]);
+    console.log(convertMs(selectedDates[0] - currentDate));
+
+    if (selectedDates[0] <= currentDate) {
+      Notiflix.Notify.failure(
+        'Вся русня здохне, але, на жаль, тільки в найближчому майбутньому. Оберіть бажану дату.',
+        {
+          timeout: 4000,
+        }
+      );
+      refs.startCountdown.disabled = true;
+    } else {
+      Notiflix.Notify.success('Запуску відліку до найбільшого свята готовий!');
+      refs.startCountdown.disabled = false;
+    }
+  },
+});
+
+function onStartCountdownClick(event) {
+  if (isActive) {
+    Notiflix.Notify.warning(
+      'Таймер вже запущено, дочекайтесь закінчення роботи поточного і повторіть спробу'
+    );
+    return;
+  }
+
+  Notiflix.Notify.info('Відлік розпочато!');
+  let timeRemain = {};
+
+  const interval = setInterval(() => {
+    isActive = true;
+    let timeDiff = selectedDate - new Date();
+    if (timeDiff >= 0) {
+      timeRemain = convertMs(timeDiff);
+      refs.days.textContent = timeRemain.days;
+      refs.hours.textContent = timeRemain.hours;
+      refs.minutes.textContent = timeRemain.minutes;
+      refs.seconds.textContent = timeRemain.seconds;
+      console.log(timeRemain);
+    } else {
+      clearInterval(interval);
+      Notiflix.Report.success('Вся русня здохла!', '', 'Ура!');
+    }
+  }, 1000);
+}
